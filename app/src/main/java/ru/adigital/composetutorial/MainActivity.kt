8d3b083ae8e0a648ac96fe60c34a91920263ddb4
adigital <adigital@yandex.ru>
2024-06-05 18:22:20 +0300

    Edit

--- app/src/main/java/ru/adigital/composetutorial/MainActivity.kt
+++ app/src/main/java/ru/adigital/composetutorial/MainActivity.kt
@@ -20,7 +20,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -47,13 +46,6 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun ClickCounter(clicks: Int, onClick: () -> Unit) {
-    Button(onClick = onClick) {
-        Text("I've been clicked $clicks times")
     }
 }
 
