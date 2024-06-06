10d9c6d4df58e5d2b641a41724488f5b81ca39a4
adigital <adigital@yandex.ru>
2024-06-06 12:06:12 +0300

    Edit

--- app/src/androidTest/java/ru/adigital/composetutorial/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ru/adigital/composetutorial/ExampleInstrumentedTest.kt
@@ -1,12 +1,15 @@
 package ru.adigital.composetutorial
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +23,22 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("ru.adigital.composetutorial", appContext.packageName)
+    }
+}
+
+@RunWith(AndroidJUnit4::class)
+class HomeTest {
+    @get:Rule(order = 1)
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun home_navigatesToAllScreens() {
+        composeTestRule.onNodeWithText("Jetpack Compose").assertIsDisplayed()
+//        composeTestRule.onNodeWithText("SLEEP").performClick()
+//        composeTestRule.onNodeWithText("Explore Properties by Destination").assertIsDisplayed()
+//        composeTestRule.onNodeWithText("EAT").performClick()
+//        composeTestRule.onNodeWithText("Explore Restaurants by Destination").assertIsDisplayed()
+//        composeTestRule.onNodeWithText("FLY").performClick()
+//        composeTestRule.onNodeWithText("Explore Flights by Destination").assertIsDisplayed()
     }
 }
